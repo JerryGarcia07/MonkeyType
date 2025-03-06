f7d53cadda2ef24c55b82599c5abb8a87ed2d838
Jerry Garcia Tanta <jerry_garcia_07@hotmail.com>
2025-03-06 00:11:35 -0500

    mostar los errores

--- index.js
+++ index.js
@@ -51,7 +51,31 @@ function initEvents() {
   $imput.addEventListener("keyup", onkeyUp);
 }
 
-function onkeyDown() {}
+function onkeyDown(event) {
+  const $currentWord = $paragraph.querySelector("word.active");
+  const $currentLetter = $currentWord.querySelector("letter.active");
+  const { key } = event;
+  if (key === " ") {
+    event.preventDefault();
+
+    const $nextWord = $currentWord.nextElementSibling;
+    const $nextLetter = $nextWord.querySelector("letter");
+
+    $currentWord.classList.remove("active");
+    $currentLetter.classList.remove("active");
+
+    $nextWord.classList.add("active");
+    $nextLetter.classList.add("active");
+
+    $imput.value = "";
+
+    const hasMissedLetters =
+      $currentWord.querySelectorAll("letter:not(.correct)").length > 0;
+
+    const classToAdd = hasMissedLetters ? "marked" : "correct";
+    $currentWord.classList.add(classToAdd);
+  }
+}
 function onkeyUp() {
   //recuperamops los elementos actuales
   const $currentWord = $paragraph.querySelector("word.active");
@@ -60,6 +84,30 @@ function onkeyUp() {
   const currentWord = $currentWord.innerText.trim();
   $imput.maxLength = currentWord.length;
   console.log({ value: $imput.value, currentWord });
+
+  const $allLetter = $currentWord.querySelectorAll("letter");
+  $allLetter.forEach(($letter) =>
+    $letter.classList.remove("correct", "incorrect")
+  );
+
+  $imput.value.split("").forEach((char, index) => {
+    const $letter = $allLetter[index];
+    const letterToCehck = currentWord[index];
+
+    const isCorrect = char === letterToCehck;
+    const letterClass = isCorrect ? "correct" : "incorrect";
+    $letter.classList.add(letterClass);
+  });
+
+  $currentLetter.classList.remove("active", "is-last");
+  const inputLeght = $imput.value.length;
+  const $nextActiveLetter = $allLetter[inputLeght];
+
+  if ($nextActiveLetter) {
+    $nextActiveLetter.classList.add("active");
+  } else {
+    $currentLetter.classList.add("active", "is-last");
+  }
 }
 
 function gameOver() {
